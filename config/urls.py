af53e04412f6ede5009e09f7d97370f07c73e64e
minaton-ru <minaton@yandex.ru>
2023-07-17 22:51:48 +0300

    default login removed

--- config/urls.py
+++ config/urls.py
@@ -4,6 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("catalog.urls")),
-    path("", include("users.urls")),
     path('accounts/', include('allauth.urls')),
 ]
