076ba96abd27fec7be1c9537dd8a0aaa11675c33
minaton-ru <minaton@yandex.ru>
2023-07-29 16:13:13 +0300

    flake8

--- catalog/models.py
+++ catalog/models.py
@@ -2,49 +2,59 @@ from django.db import models
 from django.contrib.auth.models import User
 from users.models import Profile
 
+
 class Category(models.Model):
     name = models.CharField('category', max_length=20)
     slug = models.SlugField()
-    
+
     class Meta:
         ordering = ['id']
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
-    
+
     def __str__(self):
         return self.name
+
 
 class Topic(models.Model):
     name = models.CharField('topic', max_length=20)
     slug = models.SlugField()
-    category = models.ForeignKey(Category, on_delete=models.SET_DEFAULT, null=True, default="Other")
-    
+    category = models.ForeignKey(Category, on_delete=models.SET_DEFAULT,
+                                 null=True, default="Other")
+
     class Meta:
         ordering = ['id']
         verbose_name = 'Тема'
         verbose_name_plural = 'Темы'
-    
+
     def __str__(self):
         return self.name
 
+
 class Post(models.Model):
-    author = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, blank=True)
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE,
+                               null=True, blank=True)
     text = models.TextField(max_length=2000)
     fromplace = models.TextField(max_length=50)
-    image = models.ImageField(upload_to="uploads/%Y/%m/%d/", null=True, blank=True)
-    category = models.ForeignKey(Category, on_delete=models.SET_DEFAULT, null=True, default="Other")
-    topic = models.ForeignKey(Topic, on_delete=models.SET_DEFAULT, null=True, default="Other")
+    image = models.ImageField(upload_to="uploads/%Y/%m/%d/",
+                              null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_DEFAULT,
+                                 null=True, default="Other")
+    topic = models.ForeignKey(Topic, on_delete=models.SET_DEFAULT,
+                              null=True, default="Other")
     published = models.DateTimeField(auto_now_add=True)
-    upvotes = models.ManyToManyField(User, related_name='post_upvotes', blank=True)
-    downvotes = models.ManyToManyField(User, related_name='post_downvotes', blank=True)
+    upvotes = models.ManyToManyField(User, related_name='post_upvotes',
+                                     blank=True)
+    downvotes = models.ManyToManyField(User, related_name='post_downvotes',
+                                       blank=True)
     approved = models.BooleanField(default=False)
     rejected = models.BooleanField(default=False)
-    
+
     class Meta:
         ordering = ['-published']
         verbose_name = 'Надпись'
         verbose_name_plural = 'Надписи'
-    
+
     def get_total_upvotes(self):
         return self.upvotes.count()
 
