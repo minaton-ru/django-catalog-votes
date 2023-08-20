ff3dab1600ff7416a7d9e662a0718c10097ea570
minaton-ru <minaton@yandex.ru>
2023-08-20 20:11:24 +0300

    small admin customize

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 from .models import Category, Topic, Post
 
-admin.site.register(Category)
-admin.site.register(Topic)
-admin.site.register(Post)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "slug"]
+
+
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "slug", "category"]
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["__str__", "topic", "approved", "published"]
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Topic, TopicAdmin)
+admin.site.register(Post, PostAdmin)
