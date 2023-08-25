a34e88c23b8041771ea868cf667089b444d33f34
minaton-ru <minaton@yandex.ru>
2023-08-25 23:18:30 +0300

    show user permission in profile view

--- users/views.py
+++ users/views.py
@@ -5,6 +5,9 @@ from catalog.models import Post
 
 def user_profile(request, pk):
     profile = get_object_or_404(Profile, pk=pk)
+    # Если у пользователя есть права на изменение постов
+    moderator = profile.user.has_perm('catalog.change_post')
     user_posts = Post.objects.filter(author=profile)
-    context = {'profile': profile, 'user_posts': user_posts}
+    context = {'profile': profile, 'user_posts': user_posts,
+               'moderator': moderator}
     return render(request, "users/profile.html", context)
